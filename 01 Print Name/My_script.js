e9a3dc434cca906f547df5f2dba0f177e75ea4d4
Abdul <abdulinnovative95@gmail.com>
2022-10-06 01:55:26 -0700

    Added First Assigment

---
+++ 01 Print Name/My_script.js
@@ -0,0 +1,23 @@
+
+
+//Assigment One __ Store a person’s name in a variable, and print a message to that person.
+
+        var name_Eric = "<H1>Hello Abdul Rehman, would you like to learn some Javascript today</H1>";
+           
+            // Further Below Examples For Your Reference //
+
+            // Var - We can be declear same name again & again and also we re-assign its values //
+            var name_Eric1 = "First Message, i am store your information in to var variable"
+            var name_Eric1 = "Overlapping Note Var variable - We can be declear same name again & again"               
+
+            
+            // Let - We can not be declear same name again but we re-assign its values //
+            // let name_Eric2 = "First Message, i am store your information in to var variable"
+            let name_Eric2 = "If we declear same name in let variable, console shows erroe like Identifier has already been decleared"
+            
+
+            
+            // Const - neither declear it again not can re assign its values //
+            // const name_Eric3 = "First Message, i am store your information in to var variable"
+            const name_Eric3 = "const variable neither be declear it again in any condition"
+            
