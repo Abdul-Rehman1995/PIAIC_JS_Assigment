33ceb5f93b574cbfb0b7f0a93d489688d4d1c366
Abdul <abdulinnovative95@gmail.com>
2022-10-11 03:59:38 -0700

    Added Adil Sir Class One Assigment

---
+++ Adil Sir Class Project/ClassOne/Myscript.js
@@ -0,0 +1,19 @@
+// alert('Hello Rehman')
+
+// const userName = prompt('What is your name');
+
+// console.log(userName);
+
+
+// var Sname = "Zaid";
+// var Sage = 27;
+
+// console.log(typeof Sname);
+// console.log(typeof Sage);
+
+let miles = prompt('Give Numbetr into Miles');
+let kilometer = 1.60934;
+let answer = miles * kilometer;
+
+let sentence = ("The Distance Of " + miles + " miles is equal to " + answer + " kilometers." );
+console.log(sentence);
